ba3d0dfe9967fe84830f95ab96822a26190d0f0f
kamirov <andrei.khramtsov@gmail.com>
2024-12-23 12:03:17 -0500

    Add prioritization

--- index.mjs
+++ index.mjs
@@ -36,38 +36,38 @@ export const handler = async (event) => {
     };
   }
 
-  const labeledTasks = await getTasks(labelMap[labelKey]);
-
-  console.log(
-    `Found ${labeledTasks.length} uncompleted tasks with label key ${labelKey}`
-  );
-
-  if (!labeledTasks.length) {
-    return {
-      statusCode: 404,
-      body: JSON.stringify({
-        message: `No tasks found with label key ${labelKey}`,
-      }),
-    };
-  }
-
-  const randomTask =
-    labeledTasks[Math.floor(Math.random() * labeledTasks.length)];
-  console.log("Random task:", randomTask);
-
-  const projectName = await getProjectName(randomTask.projectId);
-  const sectionName = await getSectionName(randomTask.sectionId);
-
-  const presentationTask = {
-    type: labelMap[labelKey],
-    project: projectName,
-    section: sectionName,
-    content: randomTask.content,
-    url: randomTask.url,
-    taskCount: labeledTasks.length,
-  };
-
   try {
+    const labeledTasks = await getTasks(labelMap[labelKey]);
+
+    console.log(
+      `Found ${labeledTasks.length} uncompleted tasks with label key ${labelKey}`
+    );
+
+    if (!labeledTasks.length) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          message: `No tasks found with label key ${labelKey}`,
+        }),
+      };
+    }
+
+    const randomTask =
+      labeledTasks[Math.floor(Math.random() * labeledTasks.length)];
+    console.log("Random task:", randomTask);
+
+    const projectName = await getProjectName(randomTask.projectId);
+    const sectionName = await getSectionName(randomTask.sectionId);
+
+    const presentationTask = {
+      type: labelMap[labelKey],
+      project: projectName,
+      section: sectionName,
+      content: randomTask.content,
+      url: randomTask.url,
+      taskCount: labeledTasks.length,
+    };
+
     return {
       statusCode: 200,
       body: JSON.stringify(presentationTask),
@@ -76,7 +76,7 @@ export const handler = async (event) => {
     console.error("Error fetching task:", error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: "Internal Server Error" }),
+      body: JSON.stringify({ message: `Internal Server Error - ${error}` }),
     };
   }
 };
@@ -103,6 +103,20 @@ async function getTasks(label) {
   const tasksNotScheduledInTheFuture = tasks.filter((task) => {
     return !task.due || DateTime.fromISO(task.due.date) <= today;
   });
+
+  const tasksScheduledForToday = tasksNotScheduledInTheFuture.filter((task) => {
+    if (task.due && !task.due.date) {
+      throw new Error(
+        `Task '${task.id} - ${task.content}' has no due date. Maybe Todoist changed how this field is represented in the API?`
+      );
+    }
+    return task.due && DateTime.fromISO(task.due.date).hasSame(today, "day");
+  });
+
+  // If we have tasks scheduled for today, let's prioritize those
+  if (tasksScheduledForToday.length) {
+    return tasksScheduledForToday;
+  }
 
   return tasksNotScheduledInTheFuture;
 }
